ecf2694139928db1ed6ca08c1c022cdf0c199a7c
aroberts-illumina <aroberts@illumina.com>
2016-04-21 23:49:15 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export let suite = (suiteInstance: any, fns: Function[], description: string) {
+    describe(description, () => {
+        for (let i = 0; i < fns.length; i++) {
+            let fn = fns[i];
+            fn();
+        }
+    });
+};
+
+export let test = (testMethod: Function, params: any, testDescription: string) => {
+    let description = testDescription + (params ? " " + JSON.stringify(params) : "");
+    it(description, () => {
+        testMethod();
+    });
+};
